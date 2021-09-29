37771c58a2671930b5728d3dbdf4f894c22a5ec1
Deepanshupanwar <90817042+Deepanshupanwar@users.noreply.github.com>
2021-09-29 10:58:03 +0530

    Update task2.1.cpp

--- practical04/task2/task2.1.cpp
+++ practical04/task2/task2.1.cpp
@@ -1,1 +1,19 @@
-#include<iostrram>
+#include<iostream>
+using namespace std;
+namespace first{
+void saysomething(){
+  cout<<"hello world"<<endl;
+}
+}
+namespace second{
+void saysomething(){
+  cout<<"hello world 1"<<endl;
+}
+}
+using namespace first;
+using namespace second;
+int main()
+{
+  saysomething();
+  return 0;
+}
