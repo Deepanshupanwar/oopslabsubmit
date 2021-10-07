ca4e8ab3faf4878d5b688f04d7c6acc23c44d814
Deepanshupanwar <90817042+Deepanshupanwar@users.noreply.github.com>
2021-10-07 10:46:23 +0530

    Add files via upload

---
+++ practical06/task1/task1.1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  float a=1.2;
+  int b=3;
+  int c=4;
+  string s="abcd";
+  string s2="efgh";
+ cout <<"int + int  "<<c+b<<endl;
+ cout <<"float + float  "<<a+a<<endl;
+ cout <<"int + float  "<<a+b<<endl;
+ cout <<"string + string  "<<s+s2<<endl;
+
+    return 0;
+}
---
+++ practical06/task1/task1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+   string a="abcd";
+   int b=10;
+   string c=a+b;
+   cout<<c<<;
+
+    return 0;
+}
+
