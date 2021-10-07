dd3e449be58734925fb57717c40abf51a960701c
Deepanshupanwar <90817042+Deepanshupanwar@users.noreply.github.com>
2021-10-07 21:30:31 +0530

    Add files via upload

---
+++ practical 05/task1/task1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int a=10328;
+  long b=2132423;
+  float c=10.2;
+  double d=11.234734;
+  long double e=0.32738263;
+  short f=11;
+  char g='a';
+  wchar_t h=L'あ';
+  bool i=true;
+    
+  cout<<a<<endl;
+  cout<<b<<endl;
+  cout<<c<<endl;
+  cout<<d<<endl;
+  cout<<e<<endl;
+  cout<<f<<endl;
+  cout<<g<<endl;
+  cout<<h<<endl;
+  cout<<i<<endl;
+}
---
+++ practical 05/task2/task2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int a=10328;
+  long b=2132423;
+  float c=10.2;
+  double d=11.234734;
+  long double e=0.32738263;
+  short f=11;
+  char g='a';
+  wchar_t h=L'あ';
+  bool i=true;
+   cout<<sizeof(int)<<endl;
+   cout<<sizeof(a)<<endl;
+   cout<<sizeof(long)<<endl;
+   cout<<sizeof(b)<<endl;
+   cout<<sizeof(float)<<endl;
+   cout<<sizeof(c)<<endl;
+   cout<<sizeof(double)<<endl;
+   cout<<sizeof(d)<<endl;
+   cout<<sizeof(long double)<<endl;
+   cout<<sizeof(e)<<endl;
+   cout<<sizeof(short)<<endl;
+   cout<<sizeof(f)<<endl;
+   cout<<sizeof(char)<<endl;
+   cout<<sizeof(g)<<endl;
+   cout<<sizeof(wchar_t)<<endl;
+   cout<<sizeof(h)<<endl;
+   cout<<sizeof(bool)<<endl;
+   cout<<sizeof(i)<<endl;
+
+}
