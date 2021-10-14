e166e05f6ce435ddbf63a805428af29ae64ada33
Deepanshupanwar <90817042+Deepanshupanwar@users.noreply.github.com>
2021-10-14 10:02:53 +0530

    Add files via upload

---
+++ practical 07/task7/task7.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ for(int i=1;i<=4;i++)
+ {
+   for(int j=1;j<=4;j++)
+   {
+     if(j>4-i)
+       cout<<"*";
+     else
+      cout<<" ";
+   }
+   cout<<endl;
+ }
+}
